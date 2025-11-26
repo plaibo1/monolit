313b26d13329cc185b1949c2cb20be44694ded7e
plaibo1 <playboi.2000@mail.ru>
2025-11-26 11:30:58 +0300

    feat: add proxy, dev:https

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,19 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "standalone",
+
+  async rewrites() {
+    return [
+      {
+        source: "/api/v1/auth/:path*",
+        destination: "http://5.39.222.41:8602/api/v1/auth/:path*",
+      },
+      {
+        source: "/api/v1/:path*",
+        destination: "http://5.39.222.41:8600/api/v1/:path*",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
